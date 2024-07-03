e7ae0b5535be0774e2f27c40d3194a0b50c169f4
zettlerdigital <kashifmushtaq114@gmail.com>
2024-07-03 11:55:58 +0500

    Update Kernel.php

--- web/app/Console/Kernel.php
+++ web/app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('demo:cron')->everyFiveMinutes();
+        $schedule->command('send:images')->everyFiveMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
